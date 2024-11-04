d32efdbd87ed3ed2e6a0464571ace5ddbe6e6945
odhyp <pradhana.odhy@gmail.com>
2024-11-04 10:32:43 +0700

    feat: update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+import time
+import winsound
+
+from dotenv import load_dotenv
+from playwright.sync_api import sync_playwright
+
+load_dotenv()
+
+
+def play_notification():
+    sound_path = "assets/notification.wav"
+    winsound.PlaySound(sound_path, winsound.SND_FILENAME)
+
+
 def main():
-    pass
+    username = os.getenv("SIPD_USERNAME")
+    password = os.getenv("SIPD_PASSWORD")
+
+    play_notification(5)
 
 
 if __name__ == "__main__":
