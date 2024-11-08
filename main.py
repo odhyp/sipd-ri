e03207001503421ee1fc9f2fd648acadf174d79c
odhyp <pradhana.odhy@gmail.com>
2024-11-08 10:59:06 +0700

    feat: add SIPDBot class

--- main.py
+++ main.py
@@ -16,11 +16,26 @@ def play_notification():
         pass
 
 
+class SIPDBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.browser = None
+        self.context = None
+        self.page = None
+
+    def initialize_browser(self):
+        playwright = sync_playwright().start()
+        self.browser = playwright.chromium.launch(
+            headless=False, args=["--start-maximized"]
+        )
+        self.context = self.browser.new_context(no_viewport=True)
+        self.page = self.context.new_page()
+
+
 def main():
     username = os.getenv("SIPD_USERNAME")
     password = os.getenv("SIPD_PASSWORD")
-
-    play_notification(5)
 
 
 if __name__ == "__main__":
