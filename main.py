3737835ea59d7b326f98cc6422dd96952bfa26f2
odhyp <pradhana.odhy@gmail.com>
2024-11-08 11:43:14 +0700

    feat: add login and close method

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from playwright.sync_api import sync_playwright
 
 load_dotenv()
 
+URL_SIPD_LOGIN = "https://sipd.kemendagri.go.id/penatausahaan/login"
+URL_SIPD_AKLAP = "https://sipd.kemendagri.go.id/penatausahaan/aklap"
+
 
 def play_notification():
     sound_path = "assets/notification.wav"
@@ -24,7 +27,7 @@ class SIPDBot:
         self.context = None
         self.page = None
 
-    def initialize_browser(self):
+    def _initialize_browser(self):
         playwright = sync_playwright().start()
         self.browser = playwright.chromium.launch(
             headless=False, args=["--start-maximized"]
@@ -32,10 +35,64 @@ class SIPDBot:
         self.context = self.browser.new_context(no_viewport=True)
         self.page = self.context.new_page()
 
+    def _login(self):
+        if not self.page:
+            self._initialize_browser()
+
+        # TODO: add fail-safe for bad connection
+        self.page.goto(URL_SIPD_LOGIN, timeout=60_000)
+
+        # Login Form
+        input_username = self.page.locator("#ed_username")
+        input_username.wait_for(state="visible")
+        input_username.focus()
+        input_username.type(self.username)
+        input_password = self.page.locator("#ed_password")
+        input_password.wait_for()
+        input_password.type(self.password)
+        input_password.press("Enter")
+
+        # Account card
+        card_account = self.page.locator(
+            "div.account-select-card:has-text('Bendahara Umum Daerah')"
+        )
+        btn_account = card_account.locator("button:has-text('Pilih Akun ini')")
+        card_account.wait_for()
+        btn_account.click()
+
+        # CAPTCHA form
+        # TODO: add input() to delay automation after user fill the CAPTCHA form
+        play_notification()
+
+        # Sidebar - Akuntansi
+        # TODO: change wait for a more universal element e.g. title
+        menu_link = self.page.locator('a:has-text("Akuntansi")').first
+        # TODO: add fail-safe method
+        menu_link.wait_for(timeout=60_000)
+
+        time.sleep(5)
+        print(">>>>>>>>>>>>>>>>>>>>>>HERE")
+
+    def sample(self):
+        self._login()
+
+        print("SAMPLE >>>>>>>>>>>>>>>")
+        time.sleep(5)
+
+    def close(self):
+        if self.context:
+            self.context.close()
+        if self.browser:
+            self.browser.close()
+
 
 def main():
     username = os.getenv("SIPD_USERNAME")
     password = os.getenv("SIPD_PASSWORD")
+
+    bot = SIPDBot(username=username, password=password)
+    bot.sample()
+    bot.close()
 
 
 if __name__ == "__main__":
