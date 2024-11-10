6f7228a6ca187b396194e0c473c6f1503650c17d
odhyp <pradhana.odhy@gmail.com>
2024-11-10 20:45:37 +0700

    feat: add helper_excel.py

---
+++ src/helper_excel.py
@@ -0,0 +1,12 @@
+class ExcelHelper:
+    def __init__(self):
+        pass
+
+    def compress_excel(self):
+        pass
+
+    def compile_excel(self):
+        pass
+
+    def compile_laporan_realisasi(self):
+        pass
