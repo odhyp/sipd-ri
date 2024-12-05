060402f5c8fb6c33a2d607424edcc05b9c5d431e
odhyp <pradhana.odhy@gmail.com>
2024-12-05 08:48:43 +0700

    feat: add getter for current date

--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,10 @@ def get_month_name(index):
     return month_list[index - 1]
 
 
+def get_current_time() -> str:
+    return datetime.now().strftime("%H:%M:%S")
+
+
 def get_current_date() -> str:
     return datetime.now().strftime("%Y-%m-%d")
 
