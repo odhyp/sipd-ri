e4bd22f8469fbdc409892ef4bb69f0bde2526bc9
odhyp <pradhana.odhy@gmail.com>
2024-12-05 08:53:14 +0700

    style: add docstring for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,26 @@
+"""
+Utility module for date, time, and file path operations.
+Provides common operations with getters.
+"""
+
 import os
 from pathlib import Path
 from datetime import datetime
 
 
-def get_month_name(index):
+def get_month_name(index) -> str:
+    """
+    Get the name of the month corresponding to the given index.
+
+    Args:
+        index (int): The index of the month (1 for January, 12 for December).
+
+    Returns:
+        str: The name of the month in Indonesian.
+
+    Raises:
+        IndexError: If the index is outside the range 1-12.
+    """
     month_list = [
         "Januari",
         "Februari",
@@ -22,14 +39,33 @@ def get_month_name(index):
 
 
 def get_current_time() -> str:
+    """
+    Get the current time in HH:MM:SS format.
+
+    Returns:
+        str: The current time as a string formatted as `HH:MM:SS`.
+    """
     return datetime.now().strftime("%H:%M:%S")
 
 
 def get_current_date() -> str:
+    """
+    Get the current date in YYYY-MM-DD format.
+
+    Returns:
+        str: The current date as a string formatted as `YYYY-MM-DD`.
+    """
     return datetime.now().strftime("%Y-%m-%d")
 
 
 class PathHelper:
+    """
+    A utility class for managing file paths.
+    """
+
+    @staticmethod
+    def get_current_dir() -> str:
+        return Path(os.getcwd())
 
     @staticmethod
     def get_download_path() -> str:
