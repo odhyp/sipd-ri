dff42dc2f77caca3c534d08577cd5d13235020dd
odhyp <pradhana.odhy@gmail.com>
2024-12-18 10:37:41 +0700

    style: add docstring and type hints

--- src/helper_excel.py
+++ src/helper_excel.py
@@ -5,7 +5,16 @@ class ExcelHelper:
     """ """
 
     @staticmethod
-    def read_jurnal_umum(file_path: str):
+    def read_jurnal_umum(file_path: str) -> list:
+        """
+        Read an excel file of Jurnal Umum and store the values in list.
+
+        Args:
+            file_path (str): The path to Excel file for Jurnal Umum.
+
+        Returns:
+            list: The list of Jurnal Umum.
+        """
         df = pd.read_excel(file_path)
         jurnal_umum = df.values.tolist()
         return jurnal_umum
