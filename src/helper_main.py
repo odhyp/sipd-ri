4fe2e7cede26252498809a8fa6a88ff166f8ec19
odhyp <pradhana.odhy@gmail.com>
2025-01-16 01:47:03 +0700

    feat: add utility functions

--- src/helper_main.py
+++ src/helper_main.py
@@ -14,7 +14,7 @@ from rich.traceback import Traceback
 
 from src.bot_sipd import SIPDBot
 from src.helper_excel import ExcelHelper
-from src.utils import select_excel_file, select_excel_files
+from src.utils import select_excel_file, select_excel_files, get_current_date
 
 
 def save_cookies():
