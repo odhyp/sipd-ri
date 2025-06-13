8c7c9d253adeca1a954cb2b490e7e5b7472d29f0
odhyp <pradhana.odhy@gmail.com>
2025-06-13 15:56:40 +0700

    feat: add main.py with argparse and logging

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from sipd-ri!")
+import os
+import logging
+import argparse
+from datetime import datetime
+from src.menu import run_menu
 
+# ---- CLI ARG PARSER ----
+parser = argparse.ArgumentParser(description="Run SIPD-RI Helper")
+parser.add_argument(
+    "--dev", action="store_true", help="Development mode with DEBUG logging"
+)
+args = parser.parse_args()
 
+# ---- LOGGING SETUP -----
+os.makedirs("logs", exist_ok=True)
+log_level = logging.DEBUG if args.dev else logging.INFO
+log_filename = datetime.now().strftime("logs/%Y-%m-%d.log")
+logging.basicConfig(
+    level=log_level,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(log_filename, encoding="utf-8"),
+    ],
+)
+
+# ---- MAIN EXECUTION ----
 if __name__ == "__main__":
-    main()
+    run_menu()
