3141a7a4053009e89b1ce63f6fc00ce560bdf786
odhyp <pradhana.odhy@gmail.com>
2025-06-13 09:03:27 +0700

    refactor: rewrite and update project using uv

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-"""
-A simple command-line app for SIPD-RI tasks by Odhy Pradhana
-"""
-
-from src.helper_main import run_app
-
-
 def main():
-    """
-    The entry point of the program.
-    Calls the `run_app` function to initialize the application's main menu.
-    """
-    run_app()
+    print("Hello from sipd-ri!")
 
 
 if __name__ == "__main__":
